f564c19cd3c46542575d5ef96157bc818921438f
Oskar Łukasiewicz <lukasiewiczoskar079978@gmail.com>
2022-06-09 00:20:12 +0200

    work on showing and hiding the cart

--- src/js/script.js
+++ src/js/script.js
@@ -124,6 +124,7 @@
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
       thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
+      
     }
 
     initAccordion() {
@@ -170,7 +171,7 @@
       thisProduct.amountWidgetElem.addEventListener('updated', function () {
         thisProduct.processOrder();
       });
-      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidget = new amountWidget(thisProduct.amountWidgetElem);
     }
 
     processOrder() {
@@ -218,11 +219,12 @@
 
       //update calculated price in the HTML
       price *= thisProduct.amountWidget.value;
+      
       thisProduct.priceElem.innerHTML = price;
     }
   }
 
-  class AmountWidget {
+  class amountWidget {
     constructor(element) {
       const thisWidget = this;
 
@@ -278,6 +280,36 @@
     }
   }
 
+  class Cart {
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log('new cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      })
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -293,6 +325,13 @@
 
       thisApp.data = dataSource;
     },
+    
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
 
     init: function () {
       const thisApp = this;
@@ -304,8 +343,10 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
+
 
   app.init();
 }
